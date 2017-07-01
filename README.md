331498fca2393ce652d6d1f5fd58a93a9b3b1ebf
wegamekinglc <scrappedprince.li@gmail.com>
2017-07-01 14:53:44 +0800

    update scripts

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Portfolio - Optimizer
+
+## 依赖
+
+* cmake >= 3.0.0
+* boost >= 1.58.0
+
+## 安装
+
+* Linux
+
+```bash
+./build_linux.sh
+```
--- build_linux.sh
+++ build_linux.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-export CXXFLAGS=-O3
-
 cd OpenBLAS
 
-make clean
+#make clean
 make -j8
 make install PREFIX=$PWD
 
 cd ../Ipopt
 ./configure PREFIX=$PWD --with-blas="-L$PWD/../OpenBLAS/lib -lopenblas" --with-lapack="-L$PWD/../OpenBLAS/lib -lopenblas" ADD_CFLAGS=-fopenmp ADD_FFLAGS=-fopenmp ADD_CXXFLAGS=-fopenmp
-make clean
+#make clean
 make -j8
 make install
 
@@ -18,5 +16,13 @@ cd ../eigen
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PWD/.. ..
+make uninstall
+make -j8 install
+
+cd ../../portfolio-optimizer
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/.. ..
 make clean
+make -j8
 make install
