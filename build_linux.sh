82cbb919e12977f3645980f598226efc3868a1d4
wegamekinglc <scrappedprince.li@gmail.com>
2017-07-30 12:43:00 +0000

    made linux build more smooth

--- build_linux.sh
+++ build_linux.sh
@@ -1,15 +1,44 @@
 #!/bin/sh
 
+export num_cores=`grep -c processor /proc/cpuinfo`
+
 cd OpenBLAS
 
 # make clean
-make -j8
+make -j${num_cores}
 make install PREFIX=$PWD
 
 cd ../Ipopt
+
+cd ThirdParty
+
+chmod 777 -R ASL
+chmod 777 -R Blas
+chmod 777 -R Lapack
+chmod 777 -R Mumps
+chmod 777 -R Metis
+chmod 777 -R HSL
+
+cd ASL
+./get.ASL
+
+cd ../Blas
+./get.Blas
+
+cd ../Lapack
+./get.Lapack
+
+cd ../Mumps
+./get.Mumps
+
+cd ../Metis
+./get.Metis
+
+cd ../..
+
 ./configure --prefix=$PWD --with-blas="-L$PWD/../OpenBLAS/lib -lopenblas" --with-lapack="-L$PWD/../OpenBLAS/lib -lopenblas" ADD_CFLAGS=-fopenmp ADD_FFLAGS=-fopenmp ADD_CXXFLAGS=-fopenmp
 # make clean
-make -j8
+make -j${num_cores}
 make install
 
 cd ../eigen
@@ -17,18 +46,18 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$PWD/.. ..
 make uninstall
-make -j8 install
+make -j${num_cores} install
 
 cd ../../alglib
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/.. ..
 make clean
-make -j8
+make -j${num_cores}
 
 cd ../../clp
 ./configure --prefix=$PWD
-make -j8
+make -j${num_cores}
 make install
 
 cd ../portfolio-optimizer
@@ -36,7 +65,7 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/.. ..
 make clean
-make -j8
+make -j${num_cores}
 
 cd ../bin
 ./test_suite
@@ -51,3 +80,5 @@ export CLASSPATH=$PWD:$CLASSPATH
 export CPLUS_INCLUDE_PATH=$JAVA_HOME/include/linux
 
 make
+
+echo "Portfolio - Optimizer building completed!"
