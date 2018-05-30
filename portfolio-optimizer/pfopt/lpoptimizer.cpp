c787e857ca6616efcb51c06dc12c4467f2eea400
cheng li <scrappedprince.li@gmail.com>
2018-05-30 15:03:47 +0800

    using clp matrix and row majored

--- portfolio-optimizer/pfopt/lpoptimizer.cpp
+++ portfolio-optimizer/pfopt/lpoptimizer.cpp
@@ -1,6 +1,7 @@
 #include "lpoptimizer.hpp"
 #include "utilities.hpp"
 #include "coin/ClpCholeskyMumps.hpp"
+#include "coin/ClpPackedMatrix.hpp"
 
 namespace pfopt {
     LpOptimizer::LpOptimizer(int numVariables,
@@ -16,20 +17,20 @@ namespace pfopt {
 
         numberOfProb_ = numberColumns;
        
-        std::vector<int> rows;
+        std::vector<int> cols;
         std::vector<int> lengths;
         std::vector<CoinBigIndex> starts;
         std::vector<double> elements;
 
         size_t currentSize = 0;
 
-        for(int j=0; j != numberColumns; ++j) {
+        for(int i=0; i != numberRows; ++i) {
             starts.push_back(currentSize);
-            for(int i=0; i != numberRows; ++i) {
+            for(int j=0; j != numberColumns; ++j) {
                 double value = constraintMatrix[i*(numberColumns+2) + j];
                 if(!is_close(value, 0.)) {
                     elements.push_back(value);
-                    rows.push_back(i);
+                    cols.push_back(j);
                 }
             }
             lengths.push_back(elements.size() - currentSize);
@@ -38,7 +39,8 @@ namespace pfopt {
 
         starts.push_back(currentSize);
 
-        CoinPackedMatrix matrix(true, numberRows, numberColumns, currentSize, &elements[0], &rows[0], &starts[0], &lengths[0]);
+        CoinPackedMatrix matrix_base(false, numberColumns, numberRows, currentSize, &elements[0], &cols[0], &starts[0], &lengths[0]);
+        ClpPackedMatrix matrix(matrix_base);
 
         std::vector<double> rowLower;
         std::vector<double> rowUpper;
